96b6bd56fe6d90808d8b742323ea5ec3fece1ebf
Scott B5 <busingescott@gmail.com>
2016-10-07 15:09:23 +0300

    my first commit of clinic day of both git and TDD

---
+++ tests.py
@@ -0,0 +1,19 @@
+# TDD
+__author__ = 'Scott Businge'
+
+import unittest
+
+
+class TestOperation(unittest.TestCase):
+
+    def test_sum(self):
+        pass
+
+    def test_subtract(self):
+        pass
+
+    def test_multiply(self):
+        pass
+
+    def test_divide(self):
+        pass
