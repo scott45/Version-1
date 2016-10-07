1fc11c427e2a6d1c27c1ae5c3280fcee01e03ee2
Scott B5 <busingescott@gmail.com>
2016-10-07 16:24:18 +0300

    second commit, added prime number functions

--- tests.py
+++ tests.py
@@ -2,12 +2,13 @@
 __author__ = 'Scott Businge'
 
 import unittest
+import calculator
 
 
 class TestOperation(unittest.TestCase):
 
     def test_sum(self):
-        pass
+        return self.assertEqual(calculator(5, 5) )
 
     def test_subtract(self):
         pass
