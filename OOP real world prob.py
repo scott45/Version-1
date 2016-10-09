b41c32bc78a5388fad1deda308fe52b828c62974
Scott B5 <busingescott@gmail.com>
2016-10-09 17:46:50 +0300

    modified oop

--- OOP real world prob.py
+++ OOP real world prob.py
@@ -1,12 +1,22 @@
 __author__ = 'Scott Businge'
 
 class Andela(object):
-    def __init__(self, country, ):
+
+    def __init__(self, country, city, ):
         self.country = country
+        self.city = city
 
 
-class Employee(Andela):
-    pass
+class Andelan(Andela):
+
+    def __init__(self, country, city, first, last, role, pay ):
+        super().__init__(country, city)
+        self.first = first
+        self.last = last
+        self.role = role
+        self.pay = pay
+
+
 
 
 class Fellow(Andela):
