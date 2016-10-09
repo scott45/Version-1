aad91132f8051f7f8815afac914c17c876dbd0d6
Scott B5 <busingescott@gmail.com>
2016-10-09 17:26:47 +0300

    added solution to outcome 4 / home day 1.. OOP

---
+++ OOP real world prob.py
@@ -0,0 +1,17 @@
+__author__ = 'Scott Businge'
+
+class Andela(object):
+    def __init__(self, country, ):
+        self.country = country
+
+
+class Employee(Andela):
+    pass
+
+
+class Fellow(Andela):
+    pass
+
+
+class Bootcamper(Andela):
+    pass
