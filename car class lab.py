f15ee31449ae246608a5e353e9763e31d9622d89
Scott B5 <busingescott@gmail.com>
2016-10-10 16:21:39 +0300

    commit

--- car class lab.py
+++ car class lab.py
@@ -2,19 +2,23 @@
 
 
 class Car(object):
-    def __init__(self, type, model, name):  # instantiation
-        self.type = honda
-        self.model = GM
-        self.name = General
+    def __init__(self, type='honda', model='GM', name='General'):  # instantiation
+        self.moving_speed = self.moving_speed
+        self.parked_speed = self.parked_speed
+        self.moving_man = None
+        self.num_of_doors = ''
+        self.type = type
+        self.model = model
+        self.name = name
 
     def doors(self, num_of_doors):
         pass
 
     def drive(self, moving_man):
-        pass
+        return moving_man
 
     def wheels(self, num_of_wheels):
-        pass
+        return num_of_wheels
 
-    def speed(self, num_of_doors):
-        pass
+    def speed(self, parked_speed=0, movind_speed=0):
+        return movind_speed
