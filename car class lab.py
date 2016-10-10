31634c0bd386084fc33467d063530b0b5c2fcdf7
Scott B5 <busingescott@gmail.com>
2016-10-10 16:08:22 +0300

    added car class lab

---
+++ car class lab.py
@@ -0,0 +1,20 @@
+# __author__ = 'Scott Businge'
+
+
+class Car(object):
+    def __init__(self, type, model, name):  # instantiation
+        self.type = honda
+        self.model = GM
+        self.name = General
+
+    def doors(self, num_of_doors):
+        pass
+
+    def drive(self, moving_man):
+        pass
+
+    def wheels(self, num_of_wheels):
+        pass
+
+    def speed(self, num_of_doors):
+        pass
